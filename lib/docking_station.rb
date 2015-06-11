9102ffe2e2922730d249759c4108ae574954b58b
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-11 17:15:06 +0100

    Refactor to create new provate methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,14 +8,30 @@ class DockingStation
   end
 
   def dock bike
-    fail 'Docking station full.' if @bike_stash.length >= 20 
+    fail 'Docking station full.' if full?
     @bike_stash << bike
   end
 
   def release_bike
-  	fail 'No bikes available' if @bike_stash.empty?
+  	fail 'No bikes available' if empty?
     @bike_stash.pop
   end
 
+
+
+
+
+
+
+
+  private
+
+  def empty?
+    @bike_stash.empty?
+  end
+
+  def full?
+    @bike_stash.count >= 20
+  end
 
 end
