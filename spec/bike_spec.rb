7684e687e43310565a536446a6d9dbdb50f4dfa4
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-11 19:05:03 +0100

    Add broken? and report_broken methods to Bike class

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,13 +1,15 @@
 require 'bike'
 
-describe Bike do 
-  
+describe Bike do
+
 it { is_expected.to respond_to :working? }
 
-it { is_expected.to respond_to :broken? }
+# it { is_expected.to respond_to :broken? }
 
-
+it 'can be reported as broken' do
+  subject.report_broken
+  expect(subject).to be_broken
 end
 
 
-
+end
